6b12540fe5d670bc479e6fb4570b1a2721ee6389
James Lloyd <jlloyd@redev.co.uk>
2024-12-10 16:22:04 +0000

    map assessor and employer to methods of OTJ

--- MaytasValidatedByEmployer/Program.cs
+++ MaytasValidatedByEmployer/Program.cs
@@ -2,6 +2,21 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
+static string GetValidator(string method)
+{
+    return method.ToLowerInvariant() switch
+    {
+        "classroom training" => "A",
+        "self-directed study" => "E",
+        "assignments" => "A",
+        "research" => "E",
+        "online training" => "A",
+        "job related projects" => "E",
+        "mentoring" => "E",
+        _ => "E",
+    };
+}
+
 using var driver = new ChromeDriver();
 
 try
@@ -12,12 +27,26 @@ try
     Console.WriteLine("Login to Maytas and visit the OTJ page, then press [ENTER] to continue...");
     Console.ReadLine();
 
-    var editButtons = driver.FindElements(By.XPath("//a[contains(@onclick, 'editRow')]"));
+    var rows = driver.FindElements(By.CssSelector("tr.customGridRow"));
 
-    foreach (var editButton in editButtons)
+    for (var i = 0; i < rows.Count; i++)
     {
-        ((IJavaScriptExecutor)driver).ExecuteScript(
-            "arguments[0].scrollIntoView(true); window.scrollBy(0, -window.innerHeight / 2);", editButton);
+        var row = rows[i];
+
+        var method = row.FindElement(By.CssSelector("select[fieldname='OTJT_METHOD']"));
+        var selectedMethod = new SelectElement(method);
+        
+        var editButton = row.FindElement(By.CssSelector("a[onclick*='editRow']"));
+
+        if (i >= rows.Count - 5)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+        }
+        else
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript(
+                "arguments[0].scrollIntoView(true); window.scrollBy(0, -window.innerHeight / 2);", editButton);
+        }
 
         await Task.Delay(50);
 
@@ -29,7 +58,7 @@ try
 
         var selectElement = new SelectElement(validatedBySelector);
 
-        selectElement.SelectByValue("E");
+        selectElement.SelectByValue(GetValidator(selectedMethod.SelectedOption.Text));
 
         await Task.Delay(50);
 
